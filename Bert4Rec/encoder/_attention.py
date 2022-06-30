d5e03f7387329a5710ea5b032ac9773717553e0f
Yi-Ting Li <liyitingtom@gmail.com>
2022-06-30 21:01:33 +0800

    add: encoder._attention.py for multihead attention
    The encoder._attention.py only contains the multihead attention module.

---
+++ Bert4Rec/encoder/_attention.py
@@ -0,0 +1,118 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from pydantic import BaseModel
+
+
+class ConfMultiHeadAttention(BaseModel):
+    """ConfMultiHeadAttention class
+
+    Args:
+        n_head (int): The number of attention heads.
+        d_key (int): The dimension of the key.
+        d_val (int): The dimension of the value.
+        d_model (int): The dimension of the model.
+        dropout_rate (float): The dropout rate, default is 0.0.
+        name (str): The name of the layer, default is 'multi_head_attn'.
+    """
+    n_head: int
+    d_key: int
+    d_val: int
+    d_model: int
+    dropout_rate = 0.0
+    name='multi_head_attn'
+
+class MultiHeadAttention(pl.LightningModule):
+    """MultiHeadAttention _summary_
+
+    Note:
+        `B`: batch size
+        `S`: sequence length
+        `dm`: d_model (dimension of model)
+        `dh`: d_head (dimension of head)
+        `nh`: n_head (number of heads) 
+
+    Warning:
+        Here dm = dh * nh
+
+    Args:
+        c (ConfMultiHeadAttention): The configuration of the Multi head attention.
+    """
+    def __init__(self, c: ConfMultiHeadAttention):
+        self.c = c
+        self.Wq = nn.Linear(
+            in_features=c.d_model,
+            out_features=c.d_key * c.n_head,
+        )
+        self.Wk = nn.Linear(
+            in_features=c.d_model,
+            out_features=c.d_key * c.n_head,
+        )
+        self.Wv = nn.Linear(
+            in_features=c.d_model,
+            out_features=c.d_val * c.n_head,
+        )
+        self.out = nn.Linear(
+            in_features=c.d_key * c.n_head,
+            out_features=c.d_model,
+        )
+        self.dropout = nn.Dropout(
+            p=c.dropout_rate,
+            inplace=True,
+        )
+    
+    def forward(
+        self,
+        batch_x: torch.tensor,
+        attn_bias: torch.tensor
+    ):
+        q = self.Wq(batch_x)
+        k = self.Wk(batch_x)
+        v = self.Wv(batch_x)
+
+        # q = [B, S, dm]
+        B, _, dm = q.size()
+
+        # get the dh (dimension of head) from d_model.
+        dh = dm // self.n_head
+
+        # to_shape = [B, -1, nh, dh]
+        to_shape = [B, -1, self.c.n_head, dh]
+
+        # q: [B, S, d * n] -> [B, S, nh, dh]
+        q = q.view(to_shape) 
+        # q: [B, S, nh, dh] -> [B, nh, S, dh]
+        q = torch.einsum("BSnd -> BnSd", q)
+
+        # k: [B, S, d * n] -> [B, S, nh, dh]
+        k = k.view(to_shape)
+        # k: [B, S, nh, dh] -> [B, nh, dh, S]
+        k = torch.einsum("BSnd -> BndS", k)
+
+        # v: [B, S, d * n] -> [B, S, nh, dh]
+        v = v.view(to_shape)
+        # v: [B, S, nh, dh] -> [B, nh, S, dh]
+        v = torch.einsum("BSnd -> BnSd", v)
+
+        # attn_bias: [B, nh, S, S]
+        attn_score = (q @ k) / torch.sqrt(dh)
+
+        # adding attention bias
+        if attn_bias is not None:
+            attn_score += attn_bias
+        
+        # softmax(QK)
+        h = torch.softmax(input=attn_score, dim=-1)
+
+        # Dropout
+        if self.c.dropout_rate:
+            h = self.dropout(h)
+
+        # [B, nh, S, dh] @ [B, nh, S, dv] -> [B, S, dm]
+        out = h @ v
+
+        # h: [B, nh, S, dh] -> [B, S, dm]
+        out = torch.einsum("BnSd -> BSD", out) + out # residual connection
+
+        # out: [B, S, dm]
+        return self.out(out)
