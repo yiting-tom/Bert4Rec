76bfca271a8ad82921625602c4ac3b52152cc21a
Yi-Ting Li <liyitingtom@gmail.com>
2022-07-03 00:04:31 +0800

    feat: adding the pytest-ordering and output shape assertion

--- tests/bert4rec/encoder/encoder_layer/test_encoder_layer.py
+++ tests/bert4rec/encoder/encoder_layer/test_encoder_layer.py
@@ -3,29 +3,15 @@ import torch
 from bert4rec.encoder.encoder_layer._encoder_layer import (ConfEncoderLayer,
                                                            EncoderLayer)
 
-from .test_attention import conf_multi_head_attention
-from .test_drop_residual_normalize_layer import \
-    conf_drop_residual_normalize_layer
-from .test_feed_forward_layer import conf_feed_forward_layer
 
-
-@pytest.fixture(scope='session')
-def conf_encoder_layer(
-    conf_multi_head_attention,
-    conf_drop_residual_normalize_layer,
-    conf_feed_forward_layer,
-    ) -> ConfEncoderLayer:
-
-    return ConfEncoderLayer(
-        conf_multihead_attn=conf_multi_head_attention,
-        conf_drop_residual_norm_layer=conf_drop_residual_normalize_layer,
-        conf_feed_forward_layer=conf_feed_forward_layer,
-        name='test_encoder_layer',
-    )
-    
-
+@pytest.mark.order(4)
 def test_encoder_layer_class(conf_encoder_layer: ConfEncoderLayer):
     c: ConfEncoderLayer = conf_encoder_layer
 
     el = EncoderLayer(c)
 
+    batch_x = torch.randn(4, 128, c.conf_multihead_attn.d_model)
+
+    out = el(batch_x=batch_x, attn_bias=None)
+
+    assert out.shape == (4, 128, c.conf_multihead_attn.d_model)
