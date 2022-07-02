b2152a0805a7c0b952aa615a3e04f0febeb6c113
Yi-Ting Li <liyitingtom@gmail.com>
2022-07-02 11:14:04 +0800

    add: __init__.py

---
+++ bert4rec/normalize/__init__.py
@@ -0,0 +1,3 @@
+from ._normalize_dropout_layer import (ConfNormalizeDropLayer,
+                                       NormalizeDropLayer)
+from ._normalize_layer import ConfNormalizeLayer, NormalizeLayer
