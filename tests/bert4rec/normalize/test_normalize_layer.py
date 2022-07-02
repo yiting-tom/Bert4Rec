04275b6eaeeddf6605bebb36f4931ff4c8d437c5
Yi-Ting Li <liyitingtom@gmail.com>
2022-07-03 00:03:57 +0800

    feat: adding the pytest-ordering and output shape assertion

--- tests/bert4rec/normalize/test_normalize_layer.py
+++ tests/bert4rec/normalize/test_normalize_layer.py
@@ -4,18 +4,11 @@ from bert4rec.normalize._normalize_layer import (ConfNormalizeLayer,
                                                  NormalizeLayer)
 
 
-@pytest.fixture(scope='session')
-def conf_normalize_layer() -> ConfNormalizeLayer:
-    return ConfNormalizeLayer(
-        norm_shape=768,
-        norm_eps=1e-5,
-        name='normalize_layer',
-    )
-
+@pytest.mark.order(7)
 def test_normalize_layer_class(conf_normalize_layer: ConfNormalizeLayer):
     normalize_layer = NormalizeLayer(c=conf_normalize_layer)
 
-    batch_x = torch.randn(1, 768)
+    batch_x = torch.randn(1, conf_normalize_layer.norm_shape)
     out = normalize_layer(batch_x)
 
     assert out.shape == batch_x.shape
