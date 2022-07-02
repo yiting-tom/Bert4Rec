523a13d4dc6548bbce069b216b6a7e3b1df804fc
Yi-Ting Li <liyitingtom@gmail.com>
2022-07-03 00:04:26 +0800

    feat: adding the pytest-ordering and output shape assertion

--- tests/bert4rec/encoder/encoder_layer/test_drop_residual_normalize_layer.py
+++ tests/bert4rec/encoder/encoder_layer/test_drop_residual_normalize_layer.py
@@ -4,16 +4,7 @@ from bert4rec.encoder.encoder_layer._drop_residual_normalize_layer import (
     ConfDropResidualNormalizeLayer, DropResidualNormalizeLayer)
 
 
-@pytest.fixture(scope='session')
-def conf_drop_residual_normalize_layer() -> ConfDropResidualNormalizeLayer:
-    return ConfDropResidualNormalizeLayer(
-        dropout_rate=0.1,
-        norm_shape=128,     # d_model
-        norm_eps=1e-5,
-        name='test_drop_residual_norm_layer',
-    )
-        
-
+@pytest.mark.order(3)
 def test_drop_residual_normalize_layer_class(conf_drop_residual_normalize_layer: ConfDropResidualNormalizeLayer):
     c: ConfDropResidualNormalizeLayer = conf_drop_residual_normalize_layer
 
